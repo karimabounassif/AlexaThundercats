27cbdff8524c173ecd1c0b561503c6e931361f00
kabounassif@solstice.com <kabounassif@solstice.com>
2018-09-17 13:19:40 -0500

    Initial

---
+++ src/main/java/com/thundercats/alexa/AlexaApplication.java
@@ -0,0 +1,12 @@
+package com.thundercats.alexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexaApplication.class, args);
+    }
+}
